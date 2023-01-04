762c2bcab9a96a9e5b15aa56ba14eb7ef4f23f4e
David Ordovskiy <david.ordovsky@gmail.com>
2023-01-03 22:46:11 -0500

    update nav style

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ import LinkButton from './LinkButton';
 const Navbar = () => {
   return (
     <nav className="bg-slate-400 px-5 py-5 flex justify-between w-full">
-      <h1 className="text-md md:text-xl"><Link href="/">David Ordovskiy | OrdSec</Link></h1>
+      <h1 className="text-md font-semibold md:text-xl"><Link href="/">David Ordovskiy | OrdSec</Link></h1>
       <ul className="flex items-center">
         <li><Link href="/bio">Bio</Link></li>
           <li className="ml-4">
