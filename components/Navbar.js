3b315d2a6417f983610916d67827e30ed2d74a87
David Ordovskiy <david.ordovsky@gmail.com>
2023-01-13 07:14:16 -0500

    convert Link to  tag for bio due to 404

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,8 @@ const Navbar = () => {
     <nav className="bg-slate-400 px-5 py-5 flex justify-between w-full lg:px-48">
       <h1 className="text-md font-semibold md:text-xl"><Link href="/">David Ordovskiy | OrdSec</Link></h1>
       <ul className="flex items-center">
-        <li><Link href="/bio">Bio</Link></li>
+        {/* <li><Link href="/bio">Bio</Link></li> */}
+        <li><a href="/bio">Bio</a></li>
         <li className="ml-4">
           <LinkButton dest="https://rebrand.ly/px3uw5m" value="Resume" newTab={true} />
         </li>
